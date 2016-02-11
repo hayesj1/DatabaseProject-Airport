eba76a7fb38133600c7c9638a38ebbffadd822db
J R Hayes <hayesj3@wit.edu>
2016-02-11 13:58:34 -0500

    Initial Commit

---
+++ src/group3/main/Main.java
@@ -0,0 +1,11 @@
+package group3.main;
+
+/**
+ * Created by hayesj3 on 2/11/2016.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
